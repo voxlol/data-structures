348d978989233ccd57a3bf76f60ce52c068ba976
MKS-students <mks.students@hackreactor.com>
2015-06-10 16:01:09 -0500

    Implemented addTail and removeHead methods on LinkedList.js

--- sprint-two/src/linkedList.js
+++ sprint-two/src/linkedList.js
@@ -4,9 +4,25 @@ var LinkedList = function(){
   list.tail = null;
 
   list.addToTail = function(value){
+    // Make a node
+    // Get the tail's point to point to the new node
+    // make the tail the new node
+    var newNode = Node(value);
+    if(this.head === null && this.tail === null){
+      this.head = newNode;
+      this.tail = newNode;
+    }else{
+      this.tail.next = newNode;
+      this.tail = newNode;
+    }
+
   };
 
   list.removeHead = function(){
+    // Current head point to the heads next
+    var returnVal = this.head.value;
+    this.head = this.head.next;
+    return returnVal;
   };
 
   list.contains = function(target){
