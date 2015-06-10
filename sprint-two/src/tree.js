4801c9485132d3a7b5ce5b63e055e7ffdb29e7c6
MKS-students <mks.students@hackreactor.com>
2015-06-10 16:45:24 -0500

    Worked on Tree

--- sprint-two/src/tree.js
+++ sprint-two/src/tree.js
@@ -1,25 +1,25 @@
 var Tree = function(value){
   var newTree = {};
   newTree.value = value;
+  newTree.children = [];
 
-  // your code here
-  newTree.children = null;  // fix me
-
+  _.extend(newTree, treeMethods);
   return newTree;
 };
-
-
-
-
 
 var treeMethods = {};
 
 treeMethods.addChild = function(value){
-
+  var newTree = Tree(value);
+  this.children.push(newTree);
 };
 
 treeMethods.contains = function(target){
+  if(this.value === target){
+    return true;
+  }else{
 
+  }
 };
 
 
