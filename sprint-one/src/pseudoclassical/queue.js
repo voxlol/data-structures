4f1efc1224bfc668e34fc4b950fe9d2370a0695c
MKS-students <mks.students@hackreactor.com>
2015-06-10 15:39:56 -0500

    Pseudoclassical finished

--- sprint-one/src/pseudoclassical/queue.js
+++ sprint-one/src/pseudoclassical/queue.js
@@ -1,6 +1,42 @@
-var Queue = function() {
-  // Hey! Rewrite in the new style. Your code will wind up looking very similar,
-  // but try not not reference your old code in writing the new style.
+var Queue = function(){
+
+  this.storage = {};
+  this.keys = Object.keys(this.storage);
+
 };
 
+Queue.prototype.enqueue = function(item){
+  var length = this.keys.length;
+  if(!length){
+    this.storage[0+''] = item;
+  }else{
+    this.storage[length+''] = item;
+  }
 
+  this.keys = Object.keys(this.storage);
+}
+
+Queue.prototype.size = function(){
+  var length = this.keys.length;
+  return !length ? 0 : length;
+}
+
+Queue.prototype.dequeue = function(){
+  var length = this.keys.length;
+  var retVal;
+  if(length){
+    retVal = this.storage[0+''];
+    delete this.storage[0+''];
+    for(var key in this.storage){
+      this.storage[key-1+''] = this.storage[key];
+      delete this.storage[key];
+    }
+  }
+
+  this.keys = Object.keys(this.storage);
+  return retVal;
+}
+
+/*
+queueMethods.updateKeys = function(){
+}*/
--- sprint-one/src/pseudoclassical/stack.js
+++ sprint-one/src/pseudoclassical/stack.js
@@ -1,6 +1,39 @@
 var Stack = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  this.storage = {};
+  this.keys = Object.keys(this.storage);
 };
 
+Stack.prototype.push = function(value){
+  var length = this.keys.length;
+  if(!length){
+    this.storage[0+''] = value;
+  }else{
+    this.storage[length+''] = value;
+  }
+
+  this.keys = Object.keys(this.storage);
+}
+
+Stack.prototype.pop = function(){
+  var length = this.keys.length;
+  var popped;
+
+  if(!length){
+    return null;
+  }else{
+    popped = this.storage[length-1+''];
+    delete this.storage[length-1+''];
+  }
+
+  this.keys = Object.keys(this.storage);
+  return popped;
+}
+
+Stack.prototype.size = function(){
+  if(this.keys.length === undefined)
+    return 0;
+  return this.keys.length;
+}
 
