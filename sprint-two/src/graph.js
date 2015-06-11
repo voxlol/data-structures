88e6f30a660a14926ed82977f9779f95a71b1b10
MKS-students <mks.students@hackreactor.com>
2015-06-11 10:19:28 -0500

    Implemented Graph.js

--- sprint-two/src/graph.js
+++ sprint-two/src/graph.js
@@ -1,27 +1,46 @@
 
 
 var Graph = function(){
+  this.graphInstance = {}
 };
 
 Graph.prototype.addNode = function(node){
+  if(!(node in this.graphInstance)){
+    this.graphInstance[node] = [];
+  }
 };
 
 Graph.prototype.contains = function(node){
+  return this.graphInstance[node] === undefined ? false : true;
 };
 
 Graph.prototype.removeNode = function(node){
+  var neighbors = this.graphInstance[node];
+  _.each(neighbors, function(neighbor){
+    this.graphInstance[neighbor] = _.without(this.graphInstance[neighbor], node);
+  });
+  delete this.graphInstance[node];
 };
 
 Graph.prototype.hasEdge = function(fromNode, toNode){
+  return _.contains(this.graphInstance[fromNode], toNode);
 };
 
 Graph.prototype.addEdge = function(fromNode, toNode){
+  this.graphInstance[fromNode].push(toNode);
+  this.graphInstance[toNode].push(fromNode);
 };
 
 Graph.prototype.removeEdge = function(fromNode, toNode){
+  this.graphInstance[fromNode] = _.without(this.graphInstance[fromNode], toNode);
+  this.graphInstance[toNode] = _.without(this.graphInstance[toNode], fromNode);
+
 };
 
 Graph.prototype.forEachNode = function(cb){
+  _.each(this.graphInstance, function(value, key){
+    cb(key);
+  });
 };
 
 /*
